6df1a5ee18a03cbe0ce899c4a6e57f393a4388fa
Aleck Landgraf <aleck.landgraf@airbnb.com>
2022-12-05 21:30:18 -0800

    [day6] part 1 and 2

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,38 +1,82 @@
 import run from 'aocrunner';
 
-const parseInput = (rawInput: string) => rawInput.split('\n');
+const parseInput = (rawInput: string) => rawInput;
 
-const part1 = (rawInput: string) => {
+function unique(str: string) {
+  return new Set(str).size === str.length;
+}
+
+const part1 = (rawInput: string, distinctCharacters = 4) => {
   const input = parseInput(rawInput);
 
-  return;
+  let start = 0;
+  let end = distinctCharacters;
+  while (true) {
+    if (unique(input.slice(start, end))) {
+      break;
+    }
+    start++;
+    end++;
+  }
+
+  return end;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+  return part1(rawInput, 14);
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 7,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 5,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 6,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 10,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 11,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 19,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 23,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 23,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 29,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 26,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  // onlyTests: true,
 });
