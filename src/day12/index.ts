9b0488ebfd69451b5e2fc5c4f6ad41747d60b607
Aleck Landgraf <aleck.landgraf@airbnb.com>
2022-12-13 11:58:11 -0800

    [day12] print the path

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -36,6 +36,40 @@ function createGrid(input: string[]) {
   return { grid, start, end };
 }
 
+function printGrid(grid: Node[][], path: Set<Node>) {
+  const children = new Map<Node, Node>();
+  for (let node of path) {
+    if (node.parent) {
+      children.set(node.parent, node);
+    }
+  }
+  for (let y = 0; y < grid.length; y++) {
+    let line = '';
+    for (let x = 0; x < grid[y].length; x++) {
+      if (path.has(grid[y][x])) {
+        const node = grid[y][x];
+        const child = children.get(node);
+        if (node.debug === 'S') {
+          line += 'S';
+        } else if (node.debug === 'E') {
+          line += 'E';
+        } else if (child?.x === x && child?.y === y - 1) {
+          line += '^';
+        } else if (child?.x === x && child?.y === y + 1) {
+          line += 'v';
+        } else if (child?.x === x - 1 && child?.y === y) {
+          line += '<';
+        } else if (child?.x === x + 1 && child?.y === y) {
+          line += '>';
+        }
+      } else {
+        line += '.';
+      }
+    }
+    console.log(line);
+  }
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const { start, grid, end } = createGrid(input);
@@ -47,6 +81,7 @@ const part1 = (rawInput: string) => {
     (current, neighbor) =>
       current.cost <= neighbor.cost && neighbor.cost - current.cost > 1,
   );
+  printGrid(grid, new Set(path));
   // console.log(
   //   'path',
   //   path.map(({ x, y, debug }) => `[${debug}](${x},${y})`).join(' -> '),
