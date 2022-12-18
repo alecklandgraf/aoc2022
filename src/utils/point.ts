c569960d04f0f8edaba8753f8ae70c7f8aa79a63
Aleck Landgraf <aleck.landgraf@airbnb.com>
2022-12-17 20:38:59 -0800

    point methods

--- src/utils/point.ts
+++ src/utils/point.ts
@@ -2,9 +2,43 @@ export default class Point {
   x: number;
   y: number;
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  constructor(x: number, y: number);
+  constructor(str: string);
+  constructor(xy: [number, number]);
+  constructor(x: number | string | [number, number], y?: number) {
+    if (typeof x === 'string') {
+      const [xStr, yStr] = x.replace(/[()]/g, '').split(',');
+      this.x = Number(xStr);
+      this.y = Number(yStr);
+    } else if (Array.isArray(x)) {
+      this.x = x[0];
+      this.y = x[1];
+    } else {
+      this.x = x;
+      this.y = y!;
+    }
+  }
+
+  neighbors4() {
+    return [
+      new Point(this.x - 1, this.y),
+      new Point(this.x + 1, this.y),
+      new Point(this.x, this.y - 1),
+      new Point(this.x, this.y + 1),
+    ];
+  }
+
+  neighbors8() {
+    return [
+      new Point(this.x - 1, this.y - 1),
+      new Point(this.x - 1, this.y),
+      new Point(this.x - 1, this.y + 1),
+      new Point(this.x, this.y - 1),
+      new Point(this.x, this.y + 1),
+      new Point(this.x + 1, this.y - 1),
+      new Point(this.x + 1, this.y),
+      new Point(this.x + 1, this.y + 1),
+    ];
   }
 
   toString() {
